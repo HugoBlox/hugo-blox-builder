2e2b801659cd961a861eb06902dccfbd10973c9e
George Cushen <gcushen@users.noreply.github.com>
2017-10-16 02:05:39 +0100

    publications: Add citation feature + improvements (#302 #310 #205 #286)
    * Add citation feature (#302)
    * Remove detail button since all pub. titles are now linked (#310)
    * Optimize order of pub. buttons and add more button options (#205)
    * Use noopener for external URLs that open in new tabs (#286)

--- static/js/hugo-academic.js
+++ static/js/hugo-academic.js
@@ -269,6 +269,40 @@
       // window.addEventListener('hashchange', filter_publications, false);
     }
 
+    // Load citation modal on 'Cite' click.
+    $('.js-cite-modal').click(function(e) {
+      e.preventDefault();
+      let filename = $(this).attr('data-filename');
+      let modal = $('#modal');
+      modal.find('.modal-body').load( filename , function( response, status, xhr ) {
+        if ( status == 'error' ) {
+          let msg = "Error: ";
+          $('#modal-error').html( msg + xhr.status + " " + xhr.statusText );
+        } else {
+          $('.js-download-cite').attr('href', filename);
+        }
+      });
+      modal.modal('show');
+    });
+
+    // Copy citation text on 'Copy' click.
+    $('.js-copy-cite').click(function(e) {
+      e.preventDefault();
+      // Get selection.
+      let range = document.createRange();
+      let code_node = document.querySelector('#modal .modal-body');
+      range.selectNode(code_node);
+      window.getSelection().addRange(range);
+      try {
+        // Execute the copy command.
+        document.execCommand('copy');
+      } catch(e) {
+        console.log('Error: citation copy failed.');
+      }
+      // Remove selection.
+      window.getSelection().removeRange(range);
+    });
+
     // Initialise Google Maps if necessary.
     initMap();
   });
