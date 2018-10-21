2948c93b238eea8507f9039ae24e3d62bee44827
Thom Wiggers <thomwiggers@users.noreply.github.com>
2018-10-21 21:32:44 +0200

    Load MathJax config without eval() (#735)
    This allows stricter content security policies for 
    `Content-Security-Policy` header.

---
+++ assets/js/mathjax-config.js
@@ -0,0 +1,6 @@
+window.MathJax = {
+  CommonHTML: { linebreaks: { automatic: true } },
+  tex2jax: { inlineMath: [ ['$', '$'], ['\\(','\\)'] ], displayMath: [ ['$$','$$'], ['\\[', '\\]'] ], processEscapes: false },
+  TeX: { noUndefined: { attributes: { mathcolor: 'red', mathbackground: '#FFEEEE', mathsize: '90%' } } },
+  messageStyle: 'none'
+};
