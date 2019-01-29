4c9c743ae60f8744c113fccbc83f6cc38eea2365
George Cushen <gcushen@users.noreply.github.com>
2019-01-29 15:05:15 +0000

    chore: update demo URL
    Also, fix Netlify ENV config

--- demo.sh
+++ demo.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cd exampleSite
-hugo -t "academic" --themesDir ../../ -p 1315 --i18n-warnings server
+HUGO_THEME=academic hugo --themesDir ../../ -p 1315 --i18n-warnings server
