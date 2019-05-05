93150a55e95094c2ca44695552e54926ca51635b
George Cushen <gcushen@users.noreply.github.com>
2019-05-05 20:14:39 +0100

    i18n: sync language packs against master to fix missing interface text
    The structure of some language packs is very outdated, leading to
    missing user interface text when using these packs.
    
    Developed a Python script to sync the structure of all language packs
    against the master pack (English) so that each pack has the latest set
    of localisation strings.
    
    Note that synced items will still need translating, this commit just
    ensures that all language packs have the same structure.
    
    Fix #1062

---
+++ scripts/update_bootstrap.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Script to copy Bootstrap files into project after running `npm install` to download Bootstrap.
+
+ASSETS_JS_DIR="assets/js/vendor/"
+ASSETS_SCSS_DIR="assets/sass/vendor/"
+
+mkdir -p $ASSETS_SCSS_DIR/bootstrap/
+
+cp node_modules/bootstrap/dist/js/bootstrap.min.js $ASSETS_JS_DIR
+cp -r node_modules/bootstrap/scss/* $ASSETS_SCSS_DIR/bootstrap/
+
+# cp node_modules/jquery/dist/jquery.min.js $ASSETS_JS_DIR
+# cp node_modules/popper.js/dist/umd/popper.min.js $ASSETS_JS_DIR
