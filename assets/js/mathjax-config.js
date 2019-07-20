c8832e3881087fcc984b48280c6e72e8a871ebd1
George Cushen <gcushen@users.noreply.github.com>
2019-07-20 19:44:30 +0100

    feat: revert Blackfriday math workaround
    Also see 255b5e8462ee146adb7ead4f0abf023e83911cad

--- assets/js/mathjax-config.js
+++ assets/js/mathjax-config.js
@@ -3,19 +3,8 @@ window.MathJax = {
   tex2jax: {
     inlineMath: [['$', '$'], ['\\(', '\\)']],
     displayMath: [['$$', '$$'], ['\\[', '\\]']],
-    processEscapes: false,
-    skipTags: ['script', 'noscript', 'style', 'textarea', 'pre']  // Remove `code` (required for inline math).
+    processEscapes: false
   },
   TeX: {noUndefined: {attributes: {mathcolor: 'red', mathbackground: '#FFEEEE', mathsize: '90%'}}},
   messageStyle: 'none'
 };
-
-MathJax.Hub.Queue(function() {
-  // Fix inline math wrapped in <code> tags after MathJax finishes running.
-  // This is a workaround to overcome a shortcoming of Blackfriday Markdown.
-  // See discussion in Hugo Docs.
-  let all = MathJax.Hub.getAllJax(), i;
-  for (i = 0; i < all.length; i += 1) {
-    all[i].SourceElement().parentNode.className += ' has-jax';
-  }
-});
