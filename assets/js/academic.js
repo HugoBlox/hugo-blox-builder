ea2a98a2c339017f8206cf9510c385e597a7e76b
George Cushen <gcushen@users.noreply.github.com>
2019-12-07 20:37:26 +0000

    feat: add support for Hugo v0.60 - remove Blackfriday TOC workaround

--- assets/js/academic.js
+++ assets/js/academic.js
@@ -385,14 +385,7 @@
    * --------------------------------------------------------------------------- */
 
   $(document).ready(function() {
-    // Fix Hugo's auto-generated Table of Contents.
-    //   Must be performed prior to initializing ScrollSpy.
-    $('#TableOfContents > ul > li > ul').unwrap().unwrap();
-    $('#TableOfContents').addClass('nav flex-column');
-    $('#TableOfContents li').addClass('nav-item');
-    $('#TableOfContents li a').addClass('nav-link');
-
-    // Fix Mmark task lists (remove bullet points).
+    // Fix Goldmark task lists (remove bullet points).
     $("input[type='checkbox'][disabled]").parents('ul').addClass('task-list');
 
     // Fix Mermaid.js clash with Highlight.js.
