eb953ed47581f9f35eb5e6cbca29901c67ff5ffd
George Cushen <gcushen@users.noreply.github.com>
2020-01-01 12:56:19 +0000

    demo: sync preprocessed resources for Hugo Themes Site

--- scripts/view_demo.sh
+++ scripts/view_demo.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env zsh
 
+# View the demo site on any local devices by binding the local IP on port 1315.
+# Run this script from the root Academic dir.
 HUGO_THEME=academic hugo --source exampleSite --themesDir ../../ --i18n-warnings --bind=0.0.0.0 -p 1315 --baseURL=http://0.0.0.0:1315 server
