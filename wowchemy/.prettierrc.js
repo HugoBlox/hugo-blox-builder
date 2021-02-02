9bbf6fa2a69895a505cd30588b195bba8a333b6e
George Cushen <gcushen@users.noreply.github.com>
2021-02-02 23:55:30 +0000

    feat: support Params.marketing.google_site_verification
    Also trial self-closing tags over void tags as Prettier doesn't support void tags: https://github.com/prettier/prettier/issues/5246

--- wowchemy/.prettierrc.js
+++ wowchemy/.prettierrc.js
@@ -6,4 +6,14 @@ module.exports = {
   jsxBracketSameLine: true,
   trailingComma: 'all',
   printWidth: 120,
+  overrides: [
+    {
+      files: [
+        "*.html"
+      ],
+      options: {
+        parser: "go-template"
+      }
+    }
+  ],
 };
