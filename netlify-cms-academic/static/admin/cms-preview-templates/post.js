a9e39039b36307b36365d13e05bb3b7879a372c8
Paaz CA <31186542+paazca@users.noreply.github.com>
2021-02-03 17:54:18 +0200

    Create post.js

---
+++ netlify-cms-academic/static/admin/cms-preview-templates/post.js
@@ -0,0 +1,20 @@
+var PostPreview = createClass({
+  render: function() {
+    var entry = this.props.entry;
+    var bg = this.props.getAsset('featured.jpg')
+    if(bg == 'featured.jpg') { // If the image doesn't exist then bg will equal the filename
+      bg = this.props.getAsset('featured.jpeg')
+      if(bg == 'featured.jpeg')
+        bg = this.props.getAsset('featured.png')
+    }
+    return h('div', {},
+      h('h1', {}, entry.getIn(['data', 'title'])),
+      h('p', {"className": "page-subtitle"}, entry.getIn(['data', 'subtitle'])),
+      (bg == 'featured.png' || entry.getIn(['data', 'image', 'preview_only']) == true)?'':h('img', {src: bg.toString()}), // Don't show image if doesn't exist or is preview only
+      h('div', {"className": "article-container"}, this.props.widgetFor('body'))
+    );
+  }
+});
+
+CMS.registerPreviewTemplate("posts", PostPreview);
+CMS.registerPreviewTemplate("projects", PostPreview);
