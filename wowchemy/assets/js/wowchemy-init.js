ede341fc23bbb0c1fbebe4b6c5391fe54f6086ba
George Cushen <gcushen@users.noreply.github.com>
2021-02-03 20:32:25 +0000

    feat: add Wowchemy CMS module

--- wowchemy/assets/js/wowchemy-init.js
+++ wowchemy/assets/js/wowchemy-init.js
@@ -14,5 +14,16 @@ window.wc = {
   isSiteThemeDark: wcIsSiteThemeDark,
 };
 
+// CMS authentication
+if (window.netlifyIdentity) {
+  window.netlifyIdentity.on('init', (user) => {
+    if (!user) {
+      window.netlifyIdentity.on('login', () => {
+        document.location.href = '/admin/';
+      });
+    }
+  });
+}
+
 // Initialize theme variation and set body theme class.
 initThemeVariation();
