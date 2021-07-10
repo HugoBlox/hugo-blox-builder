b90e6d38d5bce67ee7e279a4f851c9d98395bc99
Paaz CA <31186542+paazca@users.noreply.github.com>
2021-07-10 19:51:09 +0300

    Update and rename netlify-cms-academic/static/admin/cms-preview-templates/post.js to wowchemy/assets/js/cms-preview-templates/post.js

--- wowchemy/assets/js/cms-preview-templates/post.js
+++ wowchemy/assets/js/cms-preview-templates/post.js
@@ -2,7 +2,7 @@ let PostPreview = createClass({
   render: function() {
     let entry = this.props.entry;
     let bg = this.props.getAsset('featured.jpg')
-    if (bg == 'featured.jpg') { // If the image doesn't exist then bg will equal the filename
+    if (bg == 'featured.jpg') { // bg will be equal to 'featured.jpg' if the image doesn't exist
       bg = this.props.getAsset('featured.jpeg')
       if (bg == 'featured.jpeg') {
         bg = this.props.getAsset('featured.png')
@@ -16,7 +16,7 @@ let PostPreview = createClass({
       (bg == 'featured.png' || entry.getIn(['data', 'image', 'preview_only']) == true) ? '' : h('img', {
         src: bg.toString(),
         "className": "featured-image"
-      }), // Don't show image if doesn't exist or is preview only
+      }), // Don't show image if it doesn't exist or if it is preview only
       h('div', {
         "className": "article-container"
       }, this.props.widgetFor('body'))
