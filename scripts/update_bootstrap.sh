e1eddf74dca4bab1021afd6e695d33839135c38c
Mitchell Olsthoorn <mitchell.olsthoorn@outlook.com>
2021-10-28 17:06:05 +0200

    Modify the update script with the new procedure

--- scripts/update_bootstrap.sh
+++ scripts/update_bootstrap.sh
@@ -2,9 +2,7 @@
 # Script to copy Bootstrap files into project after running `yarn` to download Bootstrap.
 # Use:
 # 1. run from project root: zsh ./scripts/update_bootstrap.sh
-# 2. rename bootstrap/vendor -> _vendor
-# 3. update renamed dir in _mixins.scss to `@import "_vendor/rfs";`
-# 4. update jQuery and Popper CDN dependencies
+# 2. update the needed Bootstrap SCSS components in main.scss
 
 ASSETS_JS_DIR="wowchemy/assets/js/_vendor/"
 ASSETS_SCSS_DIR="wowchemy/assets/scss/_vendor/"
@@ -13,6 +11,3 @@ mkdir -p $ASSETS_SCSS_DIR/bootstrap/
 
 cp node_modules/bootstrap/dist/js/bootstrap.bundle.min.js $ASSETS_JS_DIR
 cp -r node_modules/bootstrap/scss/* $ASSETS_SCSS_DIR/bootstrap/
-
-# cp node_modules/jquery/dist/jquery.min.js $ASSETS_JS_DIR
-# cp node_modules/popper.js/dist/umd/popper.min.js $ASSETS_JS_DIR
