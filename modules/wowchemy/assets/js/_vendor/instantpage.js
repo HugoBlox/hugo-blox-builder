c329e54a1e1758497c490d1a87b0fa83ad31d1b0
rodrigoalcarazdelaosa <rodri.alcaraz@gmail.com>
2023-04-20 22:35:43 +0200

    feat: update JS dependencies to their latest versions (#2934)

--- modules/wowchemy/assets/js/_vendor/instantpage.js
+++ modules/wowchemy/assets/js/_vendor/instantpage.js
@@ -1,58 +1,114 @@
-/*! instant.page v5.1.0 - (C) 2019-2020 Alexandre Dieulot - https://instant.page/license */
+/*! instant.page v5.2.0 - (C) 2019-2023 Alexandre Dieulot - https://instant.page/license */
 
-let mouseoverTimer
-let lastTouchTimestamp
-const prefetches = new Set()
-const prefetchElement = document.createElement('link')
-const isSupported = prefetchElement.relList && prefetchElement.relList.supports && prefetchElement.relList.supports('prefetch')
-                    && window.IntersectionObserver && 'isIntersecting' in IntersectionObserverEntry.prototype
-const allowQueryString = 'instantAllowQueryString' in document.body.dataset
-const allowExternalLinks = 'instantAllowExternalLinks' in document.body.dataset
-const useWhitelist = 'instantWhitelist' in document.body.dataset
-const mousedownShortcut = 'instantMousedownShortcut' in document.body.dataset
+let _chromiumMajorVersionInUserAgent = null
+  , _allowQueryString
+  , _allowExternalLinks
+  , _useWhitelist
+  , _delayOnHover = 65
+  , _lastTouchTimestamp
+  , _mouseoverTimer
+  , _preloadedList = new Set()
+
 const DELAY_TO_NOT_BE_CONSIDERED_A_TOUCH_INITIATED_ACTION = 1111
 
-let delayOnHover = 65
-let useMousedown = false
-let useMousedownOnly = false
-let useViewport = false
+init()
 
-if ('instantIntensity' in document.body.dataset) {
-  const intensity = document.body.dataset.instantIntensity
+function init() {
+  const isSupported = document.createElement('link').relList.supports('prefetch')
+  // instant.page is meant to be loaded with <script type=module>
+  // (though sometimes webmasters load it as a regular script).
+  // So it’s normally executed (and must not cause JavaScript errors) in:
+  // - Chromium 61+
+  // - Gecko in Firefox 60+
+  // - WebKit in Safari 10.1+ (iOS 10.3+, macOS 10.10+)
+  //
+  // The check above used to check for IntersectionObserverEntry.isIntersecting
+  // but module scripts support implies this compatibility — except in Safari
+  // 10.1–12.0, but this prefetch check takes care of it.
 
-  if (intensity.substr(0, 'mousedown'.length) == 'mousedown') {
-    useMousedown = true
-    if (intensity == 'mousedown-only') {
-      useMousedownOnly = true
-    }
+  if (!isSupported) {
+    return
   }
-  else if (intensity.substr(0, 'viewport'.length) == 'viewport') {
-    if (!(navigator.connection && (navigator.connection.saveData || (navigator.connection.effectiveType && navigator.connection.effectiveType.includes('2g'))))) {
-      if (intensity == "viewport") {
-        /* Biggest iPhone resolution (which we want): 414 × 896 = 370944
-         * Small 7" tablet resolution (which we don’t want): 600 × 1024 = 614400
-         * Note that the viewport (which we check here) is smaller than the resolution due to the UI’s chrome */
-        if (document.documentElement.clientWidth * document.documentElement.clientHeight < 450000) {
-          useViewport = true
-        }
-      }
-      else if (intensity == "viewport-all") {
-        useViewport = true
-      }
-    }
-  }
-  else {
-    const milliseconds = parseInt(intensity)
-    if (!isNaN(milliseconds)) {
-      delayOnHover = milliseconds
-    }
-  }
-}
 
-if (isSupported) {
+  const handleVaryAcceptHeader = 'instantVaryAccept' in document.body.dataset || 'Shopify' in window
+  // The `Vary: Accept` header when received in Chromium 79–109 makes prefetches
+  // unusable, as Chromium used to send a different `Accept` header.
+  // It’s applied on all Shopify sites by default, as Shopify is very popular
+  // and is the main source of this problem.
+  // `window.Shopify` only exists on “classic” Shopify sites. Those using
+  // Hydrogen (Remix SPA) aren’t concerned.
+
+  const chromiumUserAgentIndex = navigator.userAgent.indexOf('Chrome/')
+  if (chromiumUserAgentIndex > -1) {
+    _chromiumMajorVersionInUserAgent = parseInt(navigator.userAgent.substring(chromiumUserAgentIndex + 'Chrome/'.length))
+  }
+  // The user agent client hints API is a theoretically more reliable way to
+  // get Chromium’s version… but it’s not available in Samsung Internet 20.
+  // It also requires a secure context, which would make debugging harder,
+  // and is only available in recent Chromium versions.
+  // In practice, Chromium browsers never shy from announcing "Chrome" in
+  // their regular user agent string, as that maximizes their compatibility.
+
+  if (handleVaryAcceptHeader && _chromiumMajorVersionInUserAgent && _chromiumMajorVersionInUserAgent < 110) {
+    return
+  }
+
+  const mousedownShortcut = 'instantMousedownShortcut' in document.body.dataset
+  _allowQueryString = 'instantAllowQueryString' in document.body.dataset
+  _allowExternalLinks = 'instantAllowExternalLinks' in document.body.dataset
+  _useWhitelist = 'instantWhitelist' in document.body.dataset
+
   const eventListenersOptions = {
     capture: true,
     passive: true,
+  }
+
+  let useMousedown = false
+  let useMousedownOnly = false
+  let useViewport = false
+  if ('instantIntensity' in document.body.dataset) {
+    const intensity = document.body.dataset.instantIntensity
+
+    if (intensity.startsWith('mousedown')) {
+      useMousedown = true
+      if (intensity == 'mousedown-only') {
+        useMousedownOnly = true
+      }
+    }
+    else if (intensity.startsWith('viewport')) {
+      const isNavigatorConnectionSaveDataEnabled = navigator.connection && navigator.connection.saveData
+      const isNavigatorConnectionLike2g = navigator.connection && navigator.connection.effectiveType && navigator.connection.effectiveType.includes('2g')
+      if (!isNavigatorConnectionSaveDataEnabled && !isNavigatorConnectionLike2g) {
+        if (intensity == "viewport") {
+          if (document.documentElement.clientWidth * document.documentElement.clientHeight < 450000) {
+            useViewport = true
+            // Smartphones are the most likely to have a slow connection, and
+            // their small screen size limits the number of links (and thus
+            // server load).
+            //
+            // Foldable phones (being expensive as of 2023), tablets and PCs
+            // generally have a decent connection, and a big screen displaying
+            // more links that would put more load on the server.
+            //
+            // iPhone 14 Pro Max (want): 430×932 = 400 760
+            // Samsung Galaxy S22 Ultra with display size set to 80% (want):
+            // 450×965 = 434 250
+            // Small tablet (don’t want): 600×960 = 576 000
+            // Those number are virtual screen size, the viewport (used for
+            // the check above) will be smaller with the browser’s interface.
+          }
+        }
+        else if (intensity == "viewport-all") {
+          useViewport = true
+        }
+      }
+    }
+    else {
+      const milliseconds = parseInt(intensity)
+      if (!isNaN(milliseconds)) {
+        _delayOnHover = milliseconds
+      }
+    }
   }
 
   if (!useMousedownOnly) {
@@ -71,81 +127,91 @@ if (isSupported) {
   }
 
   if (useViewport) {
-    let triggeringFunction
-    if (window.requestIdleCallback) {
-      triggeringFunction = (callback) => {
-        requestIdleCallback(callback, {
-          timeout: 1500,
-        })
-      }
-    }
-    else {
-      triggeringFunction = (callback) => {
+    let requestIdleCallbackOrFallback = window.requestIdleCallback
+    // Safari has no support as of 16.3: https://webkit.org/b/164193
+    if (!requestIdleCallbackOrFallback) {
+      requestIdleCallbackOrFallback = (callback) => {
         callback()
+        // A smarter fallback like setTimeout is not used because devices that
+        // may eventually be eligible to a Safari version supporting prefetch
+        // will be very powerful.
+        // The weakest devices that could be eligible are the 2017 iPad and
+        // the 2016 MacBook.
       }
     }
 
-    triggeringFunction(() => {
+    requestIdleCallbackOrFallback(function observeIntersection() {
       const intersectionObserver = new IntersectionObserver((entries) => {
         entries.forEach((entry) => {
           if (entry.isIntersecting) {
-            const linkElement = entry.target
-            intersectionObserver.unobserve(linkElement)
-            preload(linkElement.href)
+            const anchorElement = entry.target
+            intersectionObserver.unobserve(anchorElement)
+            preload(anchorElement.href)
           }
         })
       })
 
-      document.querySelectorAll('a').forEach((linkElement) => {
-        if (isPreloadable(linkElement)) {
-          intersectionObserver.observe(linkElement)
+      document.querySelectorAll('a').forEach((anchorElement) => {
+        if (isPreloadable(anchorElement)) {
+          intersectionObserver.observe(anchorElement)
         }
       })
+    }, {
+      timeout: 1500,
     })
   }
 }
 
 function touchstartListener(event) {
-  /* Chrome on Android calls mouseover before touchcancel so `lastTouchTimestamp`
-   * must be assigned on touchstart to be measured on mouseover. */
-  lastTouchTimestamp = performance.now()
+  _lastTouchTimestamp = performance.now()
+  // Chrome on Android triggers mouseover before touchcancel, so
+  // `_lastTouchTimestamp` must be assigned on touchstart to be measured
+  // on mouseover.
 
-  const linkElement = event.target.closest('a')
+  const anchorElement = event.target.closest('a')
 
-  if (!isPreloadable(linkElement)) {
+  if (!isPreloadable(anchorElement)) {
     return
   }
 
-  preload(linkElement.href)
+  preload(anchorElement.href, 'high')
 }
 
 function mouseoverListener(event) {
-  if (performance.now() - lastTouchTimestamp < DELAY_TO_NOT_BE_CONSIDERED_A_TOUCH_INITIATED_ACTION) {
+  if (performance.now() - _lastTouchTimestamp < DELAY_TO_NOT_BE_CONSIDERED_A_TOUCH_INITIATED_ACTION) {
     return
   }
 
-  const linkElement = event.target.closest('a')
+  if (!('closest' in event.target)) {
+    return
+    // Without this check sometimes an error “event.target.closest is not a function” is thrown, for unknown reasons
+    // That error denotes that `event.target` isn’t undefined. My best guess is that it’s the Document.
+    //
+    // Details could be gleaned from throwing such an error:
+    //throw new TypeError(`instant.page non-element event target: timeStamp=${~~event.timeStamp}, type=${event.type}, typeof=${typeof event.target}, nodeType=${event.target.nodeType}, nodeName=${event.target.nodeName}, viewport=${innerWidth}x${innerHeight}, coords=${event.clientX}x${event.clientY}, scroll=${scrollX}x${scrollY}`)
+  }
+  const anchorElement = event.target.closest('a')
 
-  if (!isPreloadable(linkElement)) {
+  if (!isPreloadable(anchorElement)) {
     return
   }
 
-  linkElement.addEventListener('mouseout', mouseoutListener, {passive: true})
+  anchorElement.addEventListener('mouseout', mouseoutListener, {passive: true})
 
-  mouseoverTimer = setTimeout(() => {
-    preload(linkElement.href)
-    mouseoverTimer = undefined
-  }, delayOnHover)
+  _mouseoverTimer = setTimeout(() => {
+    preload(anchorElement.href, 'high')
+    _mouseoverTimer = undefined
+  }, _delayOnHover)
 }
 
 function mousedownListener(event) {
-  const linkElement = event.target.closest('a')
+  const anchorElement = event.target.closest('a')
 
-  if (!isPreloadable(linkElement)) {
+  if (!isPreloadable(anchorElement)) {
     return
   }
 
-  preload(linkElement.href)
+  preload(anchorElement.href, 'high')
 }
 
 function mouseoutListener(event) {
@@ -153,28 +219,28 @@ function mouseoutListener(event) {
     return
   }
 
-  if (mouseoverTimer) {
-    clearTimeout(mouseoverTimer)
-    mouseoverTimer = undefined
+  if (_mouseoverTimer) {
+    clearTimeout(_mouseoverTimer)
+    _mouseoverTimer = undefined
   }
 }
 
 function mousedownShortcutListener(event) {
-  if (performance.now() - lastTouchTimestamp < DELAY_TO_NOT_BE_CONSIDERED_A_TOUCH_INITIATED_ACTION) {
+  if (performance.now() - _lastTouchTimestamp < DELAY_TO_NOT_BE_CONSIDERED_A_TOUCH_INITIATED_ACTION) {
     return
   }
 
-  const linkElement = event.target.closest('a')
+  const anchorElement = event.target.closest('a')
 
   if (event.which > 1 || event.metaKey || event.ctrlKey) {
     return
   }
 
-  if (!linkElement) {
+  if (!anchorElement) {
     return
   }
 
-  linkElement.addEventListener('click', function (event) {
+  anchorElement.addEventListener('click', function (event) {
     if (event.detail == 1337) {
       return
     }
@@ -183,54 +249,82 @@ function mousedownShortcutListener(event) {
   }, {capture: true, passive: false, once: true})
 
   const customEvent = new MouseEvent('click', {view: window, bubbles: true, cancelable: false, detail: 1337})
-  linkElement.dispatchEvent(customEvent)
+  anchorElement.dispatchEvent(customEvent)
 }
 
-function isPreloadable(linkElement) {
-  if (!linkElement || !linkElement.href) {
+function isPreloadable(anchorElement) {
+  if (!anchorElement || !anchorElement.href) {
     return
   }
 
-  if (useWhitelist && !('instant' in linkElement.dataset)) {
+  if (_useWhitelist && !('instant' in anchorElement.dataset)) {
     return
   }
 
-  if (!allowExternalLinks && linkElement.origin != location.origin && !('instant' in linkElement.dataset)) {
+  if (anchorElement.origin != location.origin) {
+    let allowed = _allowExternalLinks || 'instant' in anchorElement.dataset
+    if (!allowed || !_chromiumMajorVersionInUserAgent) {
+      // Chromium-only: see comment on “restrictive prefetch”
+      return
+    }
+  }
+
+  if (!['http:', 'https:'].includes(anchorElement.protocol)) {
     return
   }
 
-  if (!['http:', 'https:'].includes(linkElement.protocol)) {
+  if (anchorElement.protocol == 'http:' && location.protocol == 'https:') {
     return
   }
 
-  if (linkElement.protocol == 'http:' && location.protocol == 'https:') {
+  if (!_allowQueryString && anchorElement.search && !('instant' in anchorElement.dataset)) {
     return
   }
 
-  if (!allowQueryString && linkElement.search && !('instant' in linkElement.dataset)) {
+  if (anchorElement.hash && anchorElement.pathname + anchorElement.search == location.pathname + location.search) {
     return
   }
 
-  if (linkElement.hash && linkElement.pathname + linkElement.search == location.pathname + location.search) {
-    return
-  }
-
-  if ('noInstant' in linkElement.dataset) {
+  if ('noInstant' in anchorElement.dataset) {
     return
   }
 
   return true
 }
 
-function preload(url) {
-  if (prefetches.has(url)) {
+function preload(url, fetchPriority = 'auto') {
+  if (_preloadedList.has(url)) {
     return
   }
 
-  const prefetcher = document.createElement('link')
-  prefetcher.rel = 'prefetch'
-  prefetcher.href = url
-  document.head.appendChild(prefetcher)
+  const linkElement = document.createElement('link')
+  linkElement.rel = 'prefetch'
+  linkElement.href = url
 
-  prefetches.add(url)
+  linkElement.fetchPriority = fetchPriority
+  // By default, a prefetch is loaded with a low priority.
+  // When there’s a fair chance that this prefetch is going to be used in the
+  // near term (= after a touch/mouse event), giving it a high priority helps
+  // make the page load faster in case there are other resources loading.
+  // Prioritizing it implicitly means deprioritizing every other resource
+  // that’s loading on the page. Due to HTML documents usually being much
+  // smaller than other resources (notably images and JavaScript), and
+  // prefetches happening once the initial page is sufficiently loaded,
+  // this theft of bandwidth should rarely be detrimental.
+
+  linkElement.as = 'document'
+  // as=document is Chromium-only and allows cross-origin prefetches to be
+  // usable for navigation. They call it “restrictive prefetch” and intend
+  // to remove it: https://crbug.com/1352371
+  //
+  // This document from the Chrome team dated 2022-08-10
+  // https://docs.google.com/document/d/1x232KJUIwIf-k08vpNfV85sVCRHkAxldfuIA5KOqi6M
+  // claims (I haven’t tested) that data- and battery-saver modes as well as
+  // the setting to disable preloading do not disable restrictive prefetch,
+  // unlike regular prefetch. That’s good for prefetching on a touch/mouse
+  // event, but might be bad when prefetching every link in the viewport.
+
+  document.head.appendChild(linkElement)
+
+  _preloadedList.add(url)
 }
