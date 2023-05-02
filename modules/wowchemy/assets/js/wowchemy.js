55cd980a3480c24c92c5b05ced008af7a985401c
Agos95 <agostini.federico.95@gmail.com>
2023-05-02 15:59:04 +0200

    finish migration guide v4 to v5

--- modules/wowchemy/assets/js/wowchemy.js
+++ modules/wowchemy/assets/js/wowchemy.js
@@ -270,7 +270,9 @@ $(window).on('load', function () {
   }
 
   // Init. author notes (tooltips).
-  $('[data-toggle="tooltip"]').tooltip();
+  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
 });
 
 // Theme chooser events.
