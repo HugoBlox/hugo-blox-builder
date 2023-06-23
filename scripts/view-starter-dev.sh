aa0bd9f032da47a22d194c5827bcf5c96a26a48d
Agos95 <agostini.federico.95@gmail.com>
2023-06-23 12:56:22 +0200

    fix view-starter-dev script

--- scripts/view-starter-dev.sh
+++ scripts/view-starter-dev.sh
@@ -4,6 +4,8 @@ HUGOxPARAMSxCMSxLOCAL_BACKEND=true \
 HUGO_MODULE_REPLACEMENTS="github.com/wowchemy/wowchemy-hugo-themes/modules/wowchemy/v5 -> ../../../modules/wowchemy,
 github.com/wowchemy/wowchemy-hugo-themes/modules/wowchemy-plugin-netlify -> ../../../modules/wowchemy-plugin-netlify,
 github.com/wowchemy/wowchemy-hugo-themes/modules/wowchemy-plugin-reveal -> ../../../modules/wowchemy-plugin-reveal,
-github.com/wowchemy/wowchemy-hugo-themes/modules/wowchemy-plugin-netlify-cms -> ../../../modules/wowchemy-plugin-netlify-cms" \
+github.com/wowchemy/wowchemy-hugo-themes/modules/wowchemy-plugin-netlify-cms -> ../../../modules/wowchemy-plugin-netlify-cms,
+github.com/wowchemy/wowchemy-hugo-themes/modules/wowchemy-seo -> ../../../modules/wowchemy-seo,
+github.com/wowchemy/wowchemy-hugo-themes/modules/wowchemy-core -> ../../../modules/wowchemy-core" \
 hugo server --panicOnWarning --renderStaticToDisk -F --port 8081 --bind 0.0.0.0 \
 --source "starters/$1"
--- starters/research-group/go.mod
+++ starters/research-group/go.mod
@@ -3,7 +3,9 @@ module github.com/wowchemy/starter-hugo-research-group
 go 1.15
 
 require (
+	github.com/wowchemy/wowchemy-hugo-themes/modules/wowchemy-core v0.2.0 // indirect
 	github.com/wowchemy/wowchemy-hugo-themes/modules/wowchemy-plugin-netlify v1.0.0 // indirect
 	github.com/wowchemy/wowchemy-hugo-themes/modules/wowchemy-plugin-netlify-cms v1.0.0 // indirect
+	github.com/wowchemy/wowchemy-hugo-themes/modules/wowchemy-seo v0.1.0 // indirect
 	github.com/wowchemy/wowchemy-hugo-themes/modules/wowchemy/v5 v5.7.1-0.20230418194707-ca2775cd57a2 // indirect
 )
