bf2aae99abd82648eaec5e176e2a082e67a9a41f
George Cushen <gcushen@users.noreply.github.com>
2023-11-13 18:34:53 +0000

    feat(blox-tailwind): add skills, languages, experience blox

---
+++ modules/blox-tailwind/assets/js/katex-config.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+  renderMathInElement(document.body, {
+    delimiters: [
+      { left: "$$", right: "$$", display: true },
+      { left: "$", right: "$", display: false },
+      { left: "\\(", right: "\\)", display: false },
+      { left: "\\[", right: "\\]", display: true },
+    ],
+    throwOnError: false,
+  });
+});
