358962ce08504395c2a67d17d93497ce8e771003
George Cushen <gcushen@users.noreply.github.com>
2023-11-16 07:57:40 +0000

    feat(tailwind): port brand icons, tags page, search, Awards block, card zoom

--- scripts/view-starter-dev.sh
+++ scripts/view-starter-dev.sh
@@ -15,16 +15,18 @@ trap "exitfn" INT            # Set up SIGINT trap to call function.
 # due to Hugo limitation requiring Hugo to be run from site dir
 cd "starters/$1"
 
-WC_DEMO=true \
-HUGO_ENVIRONMENT=development \
-HUGOxPARAMSxCMSxLOCAL_BACKEND=true \
-HUGO_MODULE_REPLACEMENTS="github.com/HugoBlox/hugo-blox-builder/modules/blox-bootstrap/v5 -> ../../../modules/blox-bootstrap,
+export WC_DEMO=true \
+export HUGO_ENVIRONMENT=development \
+export HUGOxPARAMSxDECAP_CMSxLOCAL_BACKEND=true \
+export HUGO_MODULE_REPLACEMENTS="github.com/HugoBlox/hugo-blox-builder/modules/blox-bootstrap/v5 -> ../../../modules/blox-bootstrap,
 github.com/HugoBlox/hugo-blox-builder/modules/blox-plugin-netlify -> ../../../modules/blox-plugin-netlify,
 github.com/HugoBlox/hugo-blox-builder/modules/blox-plugin-reveal -> ../../../modules/blox-plugin-reveal,
 github.com/HugoBlox/hugo-blox-builder/modules/blox-tailwind -> ../../../modules/blox-tailwind,
 github.com/HugoBlox/hugo-blox-builder/modules/blox-plugin-decap-cms -> ../../../modules/blox-plugin-decap-cms,
 github.com/HugoBlox/hugo-blox-builder/modules/blox-core -> ../../../modules/blox-core,
 github.com/HugoBlox/hugo-blox-builder/modules/blox-seo -> ../../../modules/blox-seo" \
+hugo && \
+npm_config_yes=true npx pagefind --site "public" --output-subdir ../static/pagefind && \
 hugo server --panicOnWarning --renderStaticToDisk -F --port 8081 --bind 0.0.0.0
 
 trap SIGINT                  # Restore signal handling to previous before exit.
